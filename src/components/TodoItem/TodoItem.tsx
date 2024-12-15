856c8aaa7a7794e22e5dd17b7c3bc9a31cfb8958
Vasyl Pylypchynets <pylypchynets.vasyl@gmail.com>
2024-12-15 17:01:06 +0200

    solution

---
+++ src/components/TodoItem/TodoItem.tsx
@@ -0,0 +1,59 @@
+import { Todo } from '../../types/Todo';
+
+type TodoItemProps = {
+  onDeleteTodo: (id: number) => void;
+  todo: Todo;
+  isLoadingChange: boolean;
+  deleteTodoId: number | null;
+  cleanCompleted: boolean;
+  isAdding?: boolean;
+};
+
+export function TodoItem({
+  todo,
+  onDeleteTodo,
+  isLoadingChange,
+  deleteTodoId,
+  cleanCompleted,
+  isAdding,
+}: TodoItemProps) {
+  return (
+    <div data-cy="Todo" className={`todo ${todo.completed ? 'completed' : ''}`}>
+      <label htmlFor={`${todo.id}`} className="todo__status-label">
+        {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+        <input
+          id={`${todo.id}`}
+          data-cy="TodoStatus"
+          type="checkbox"
+          className="todo__status"
+          checked={todo.completed}
+        />
+      </label>
+
+      <span data-cy="TodoTitle" className="todo__title">
+        {todo.title}
+      </span>
+
+      {/* Remove button appears only on hover */}
+
+      <button
+        type="button"
+        className="todo__remove"
+        data-cy="TodoDelete"
+        onClick={() => onDeleteTodo(todo.id)}
+      >
+        ×
+      </button>
+
+      {/* Overlay will cover the todo while it is being deleted or updated */}
+      <div
+        data-cy="TodoLoader"
+        className={`modal overlay ${(isLoadingChange && deleteTodoId === todo.id) || (cleanCompleted && todo.completed) || isAdding ? 'is-active' : ''}`}
+        key={todo.id}
+      >
+        <div className="modal-background has-background-white-ter" />
+        <div className="loader" />
+      </div>
+    </div>
+  );
+}
